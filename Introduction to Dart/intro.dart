d843e4ccf3550f8fadb923cce566b3fe548aae14
Coding With Kien <duongtrungkien.03052003@gmail.com>
2024-09-13 11:43:30 +0700

    first commit

---
+++ Introduction to Dart/intro.dart
@@ -0,0 +1,18 @@
+// Variables
+var name = "KienTech";
+var year = 2024;
+var antennaDiameter = 3.7;
+var flybyObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+var image = {
+  'tags': ['saturn'],
+  'url': '//path/to/saturn.jpg'
+};
+main() {
+// Hello World
+  print("Hello world!");
+
+  print(name);
+  print(year);
+  print(antennaDiameter);
+  print(image);
+}
